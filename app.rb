1b354fa7dd8138f7fc06cb01e298525706f9f1ba
Brian Schonecker <bschonec@gmail.com>
2017-04-13 14:02:43 -0400

    Updated to skeleton

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ loop do
   socket = server.accept
   request = socket.gets
 
-  steg = Cow.new({ :cow => 'stegosaurus' })
+  steg = Cow.new({ :cow => 'skeleton' })
   puts steg.say(FortuneGem.give_fortune({:max_length => 80}))
   response = steg.say FortuneGem.give_fortune({:max_length => 80})
   socket.print "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n" +
